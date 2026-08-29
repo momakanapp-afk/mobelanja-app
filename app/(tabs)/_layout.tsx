a5e56e39a2b447090c710800cad273017567aef5
momakanapp-afk <momakanapp@gmail.com>
2026-08-29 16:44:49 +0700

    Commit tabbar ready

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { useAuth } from "@clerk/expo"
 import { Ionicons } from "@expo/vector-icons"
 import { Redirect, Tabs } from 'expo-router'
 import React from 'react'
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 
 const TabsLayout = () => {
 
@@ -9,29 +10,53 @@ const TabsLayout = () => {
 
   if (!isLoaded) return null;
   if (!isSignedIn) return <Redirect href="../(auth)" />;
+  const insets  = useSafeAreaInsets();
   
   return (
-    <Tabs>
+    <Tabs
+    screenOptions={
+      {
+        headerShown:false,
+        tabBarActiveTintColor: "#1DB954",
+        tabBarInactiveTintColor: "#B3B3B3",
+        tabBarStyle: {
+          position: 'absolute',
+        backgroundColor:'#202020',
+          borderTopWidth: 0,
+          height:  38 + insets.bottom,
+          paddingTop: 4,
+          marginHorizontal: 70,
+          marginBottom: insets.bottom,
+          borderRadius: 16,
+          overflow: "hidden"
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: 600
+        },
+      }
+    }
+    >
 
       <Tabs.Screen 
         name='index' 
         options={{
           title: 'Shop',
-          tabBarIcon: ({color,size}) => <Ionicons name="grid" />
+          tabBarIcon: ({color,size}) => <Ionicons name="grid" size={size} color={color} />
         }}
       />
       <Tabs.Screen 
         name='cart' 
         options={{
           title: 'Cart',
-          tabBarIcon: ({color,size}) => <Ionicons name="cart" />
+          tabBarIcon: ({color,size}) => <Ionicons name="cart" size={size} color={color} />
         }}
       />
       <Tabs.Screen 
         name='profile' 
         options={{
           title: 'Profil',
-          tabBarIcon: ({color,size}) => <Ionicons name="person" />
+          tabBarIcon: ({color,size}) => <Ionicons name="person" size={size} color={color} />
         }}
       />
 
